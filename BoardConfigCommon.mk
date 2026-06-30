547e810c826e63bd905a5801ac65e3c662f8febd
abhi128922 <abhijithjollythayyil.com>
2026-06-30 01:14:28 +0000

    Reverted flashlight commits

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -246,6 +246,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm8550-common/BoardConfigVendor.mk
-
-# Camera
-TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.oplus`
